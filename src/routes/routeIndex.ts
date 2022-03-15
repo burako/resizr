9b68a58b1a389cfa06d013370bc62c537ae1676c
burako <burakokumus3@gmail.com>
2022-03-15 17:17:53 +0000

    reviewed fixes
    corrected dev-dependencies
    added parameter and return types to route functions

--- src/routes/routeIndex.ts
+++ src/routes/routeIndex.ts
@@ -2,11 +2,11 @@ import express from 'express';
 import resizePic from '../utilities/resizePic';
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
+routes.get('/', (req : express.Request, res : express.Response) : void => {
   res.send('this is the root path');
 });
 
-routes.get('/images', resizePic.resize, (req, res) => {
+routes.get('/images', resizePic.resize, (req : express.Request, res : express.Response) : void => {
   //accesses middleware to transform or serve an image
 });
 
